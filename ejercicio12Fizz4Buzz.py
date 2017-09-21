b1d4389c51af4672b6b9924105f8de57c823b7d8
eterX <leandro.batlle@gmail.com>
2017-09-21 20:21:31 -0300

    tercera entrega: Ernesto: "como probarías el programa en forma de tests unitarios"

--- ejercicio12Fizz4Buzz.py
+++ ejercicio12Fizz4Buzz.py
@@ -3,7 +3,11 @@
 # and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 
-for mival in range(1,101):
+def unidad(mival):
+    """
+
+    return: str según enunciado
+    """
     mistr=""
     if mival % 3 == 0:
         mistr="Fizz"
@@ -11,6 +15,43 @@ for mival in range(1,101):
         mistr+="Buzz"
 
     if mistr != "":
-        print(mistr)
+        result=mistr
     else:
-        print(str(mival))
+        result=str(mival)
+
+    return result
+
+
+import unittests
+
+
+class prueba(unittests.TestCase):
+    def enunciado1(entrada,salida):
+        """
+        pruyeba contra multiplo de 3
+        return: bool (OK?)
+        """
+        return entrada % 3 == 0
+
+    def enunciado2(entrada,salida):
+        """
+        pruyeba contra multiplo de 5
+        return: bool (OK?)
+        """
+        return entrada % 5 == 0
+
+    def enunciado3(entrada,salida):
+        """
+        pruyeba contra número no múltiplo de (3 o 5)
+        return: bool (OK?)
+        """
+        return entrada % 5 == 0
+
+
+
+    def test_entrevista():
+
+        self.assertTrue( enunciado1(linea) && enunciado2(linea) && enunciado3(linea))
+
+
+
